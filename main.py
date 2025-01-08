2d6c9d9aff600c5d10a707b98ff280b60427e815
Yongwei <128826919+Feynnnman@users.noreply.github.com>
2025-01-08 16:44:01 -0500

    main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from electrical_conductivity import Sigma
 from water_content import Theta
 
 # Streamlit interface
-st.logo("logo.png", use_container_width=True)
+st.siderbar.image("logo.png", use_container_width=True)
 
 st.sidebar.header("Upload Your Data")
 uploaded_file = st.sidebar.file_uploader("Select a file to upload")
