ec1e33557371df06a137fcc8afe00a0a24fa25ce
Yongwei <128826919+Feynnnman@users.noreply.github.com>
2025-01-10 07:23:53 -0500

    main.py

--- main.py
+++ main.py
@@ -145,7 +145,7 @@ if uploaded_file:
             # Ensure all parameters are provided
             if all(value is not None for value in input_heat_parameters.values()):
                 st.write("Processing computations...")
-                results = Heat(heat_data, parameters=input_heat_parameters)
+                results = Heat(path, parameters=input_heat_parameters)
     
     elif analysis_type == "Electrical Conductivity":
         st.header("Electrical Conductivity Analysis")
